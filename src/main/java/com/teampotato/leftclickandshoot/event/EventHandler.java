dcfcaa26ccd2ce81bb231044826e3ff065fc75b9
Kasualix <670067575@qq.com>
2023-09-24 20:56:22 +0800

    ranged weapon only

--- src/main/java/com/teampotato/leftclickandshoot/event/EventHandler.java
+++ src/main/java/com/teampotato/leftclickandshoot/event/EventHandler.java
@@ -4,7 +4,7 @@ import com.teampotato.leftclickandshoot.api.LeftClickUser;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TridentItem;
+import net.minecraft.world.item.ProjectileWeaponItem;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -16,9 +16,9 @@ public class EventHandler {
     public static void onLeftClickItem(PlayerInteractEvent.@NotNull LeftClickEmpty event) {
         ItemStack itemStack = event.getItemStack();
         Item item = itemStack.getItem();
-        if (!itemStack.isEmpty() && !(item instanceof TridentItem)) {
+        if (item instanceof ProjectileWeaponItem) {
             //This event is clientside only, so we need to get the player on the server.
-            LeftClickUser player = ((LeftClickUser) ServerLifecycleHooks.getCurrentServer().getPlayerList().getPlayer(event.getEntity().getUUID()));
+            LeftClickUser player = (LeftClickUser) ServerLifecycleHooks.getCurrentServer().getPlayerList().getPlayer(event.getEntity().getUUID());
             if (player != null) {
                 player.leftClickAndShoot$setUsingStack(itemStack);
                 player.leftClickAndShoot$setUsingItem(item);
--- src/main/java/com/teampotato/leftclickandshoot/mixin/ItemStackMixin.java
+++ src/main/java/com/teampotato/leftclickandshoot/mixin/ItemStackMixin.java
@@ -5,7 +5,7 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TridentItem;
+import net.minecraft.world.item.ProjectileWeaponItem;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -19,7 +19,7 @@ public abstract class ItemStackMixin {
 
     @Inject(method = "releaseUsing", at = @At("HEAD"), cancellable = true)
     private void disableVanillaLogic(Level pLevel, LivingEntity pLivingEntity, int pTimeLeft, CallbackInfo ci) {
-        if (pLivingEntity instanceof Player player && !(this.getItem() instanceof TridentItem) && ((LeftClickUser)player).leftClickAndShoot$getUsingStack() == null) {
+        if (pLivingEntity instanceof Player player && this.getItem() instanceof ProjectileWeaponItem && ((LeftClickUser)player).leftClickAndShoot$getUsingStack() == null) {
             ci.cancel();
         }
     }
